f33c276934e697ee6acbf1dfd053f72987eab0ab
Marcy Montross <marcy.montross@gmail.com>
2019-11-18 12:38:32 -0600

    tidy up a bit

--- src/App.js
+++ src/App.js
@@ -41,8 +41,6 @@ class App extends React.Component {
     }
 
     this.setState({
-      // currentConditions: currentConditions,
-      // city: city,
       days: days
     });
   };
@@ -77,23 +75,11 @@ class App extends React.Component {
   .then(data => {
     const currentWeather = data[0];
     const forecast = data[1];
-    
-    console.log('currentWeather', currentWeather);
     this.updateCurrentConditionsState(currentWeather)
     this.updateForcastState(forecast);
   })
+  return true; // hacky, i know...
 }
-
-  // makeApiCall = async city => {
-  //   const api_data = await fetch(
-  //     `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&APPID=${process.env.REACT_APP_API_KEY}`
-  //   ).then(resp => resp.json());
-
-    // if (api_data.cod === '200') {
-    //   await this.updateState(api_data);
-    //   return true;
-    // } else return false;
-  // };
 
   // returns array with Indices of the next five days in the list
   // from the API data (every day at 12:00 pm)
